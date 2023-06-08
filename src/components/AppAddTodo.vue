395540282f46c8256b4619007de1038e39084be0
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-06-08 14:46:11 +0500

    show add form

--- src/components/AppAddTodo.vue
+++ src/components/AppAddTodo.vue
@@ -1,10 +1,7 @@
 <template>
   <section class="add-todo">
-    <button class="add-todo__show-form-button">
-      <i class="bi bi-plus-lg"></i>
-    </button>
-    <form class="add-todo__form">
-      <button class="close-button" type="button">
+    <form v-if="isFormVisible" class="add-todo__form">
+      <button class="close-button" type="button" @click="showForm">
         <i class="bi bi-x"></i>
       </button>
       <div class="text-input text-input--focus">
@@ -12,5 +9,29 @@
       </div>
       <button class="button button--filled">Add task</button>
     </form>
+    <button v-else @click="showForm" class="add-todo__show-form-button">
+      <i class="bi bi-plus-lg"></i>
+    </button>
   </section>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+interface State {
+  isFormVisible: boolean
+}
+
+export default defineComponent({
+  data(): State {
+    return {
+      isFormVisible: false
+    }
+  },
+  methods:{
+    showForm(){
+      this.isFormVisible = !this.isFormVisible
+    }
+  }
+})
+</script>
